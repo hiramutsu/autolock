605bb5bbd2ec481978c219a2f28bf0dcf4837489
Patipan <boonggee@hotmail.com>
2013-01-29 23:57:12 -0500

    First try commit through Apps
    First try on committing. Code tested.
    I believed Ram has the working version of hall effect code.
    I can commit the code if needed.

---
+++ software/Patipan/xbeecon.h
@@ -0,0 +1,25 @@
+﻿#ifndef XBEECON_H
+#define XBEECON_H
+
+
+//This is for using arduino library to read.
+#define XBEE_SLP 3  //PD3
+#define XBEE_CTS  2 //PD2
+
+/*
+@desc Read whether the xbee is properly waken up and ready for data transmission. 
+
+@output
+	int -- 1 clear(pin low), 0 not ready (pin high), -1 error
+*/
+int xbee_cts();
+
+/*
+@desc put xbee into sleep.
+@input
+	int slp -- 0 = wake up, sleep otherwise
+*/
+void xbee_sleep(char slp);
+
+
+#endif
